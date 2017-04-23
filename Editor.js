c4f6517779cddf6df21852ccc645fcf0b4e2d0b7
Daniel Victoriano <victoriano518@gmail.com>
2017-04-23 17:29:11 -0400

    Event listeners added

--- Editor.js
+++ Editor.js
@@ -1,18 +1,21 @@
 import DOMInterface from './Dom';
-import tinyMCE from './TinyMCE';
+import TinymceInterface from './TinyMCE';
 
-var DOM = new DOMInterface();
+var DOM = new DOMInterface(); // NOTE: Should tinyMCE just inherit DOMInterface?
+var MCE = new TinymceInterface(); // NOTE: Should TextEditor just inherit TinyMCE?
 
 function TextEditor(tinymceInstance) {
-  this.editorId = tinymceInstance.id;
+  this.instance = tinymceInstance;
+  this.editorId = escapeCharacters(tinymceInstance.id);
   this.tableRowNode = DOM.getChild(`#${this.editorId}_toolbar2 tr`, 0, document);
   // this.variableColumnId = 'variableContentColumn';
+  // NOTE: Add constants/disctionary?
 }
 
-// NOTE: Make a "Menu building" class that inherits Editor??
+// NOTE: Make a "Menu building" object/class? Separate this more granular functions?
 TextEditor.prototype.buildMenu = function (iconsList) {
   var topLevel = DOM.makeNode( this.__buildToplevel__(iconsList) );
-  DOM.setStyle({ // NOTE: Bring out to pre-defined "Styles" object?? Less clutter...
+  DOM.setStyle({ // NOTE: Make reusable "Styles" object?
     width: '120px',
     'max-width': '120px'
     }, topLevel.querySelector("select"));
@@ -29,27 +32,29 @@ TextEditor.prototype.buildMenu = function (iconsList) {
     this.__appendNode__(this.tableRowNode, subMenu);
   }, this);
 
-  // Add event listeners NOTE: Clean this up later
-  var topLevelSelect = DOM.getChild('#main-style-select select', 0, this.tableRowNode);
-  var menuList = DOM.getChildren('.tinymce-menu', this.tableRowNode);
+  // Add event listeners
   var that = this; // NOTE: Better way to pass 'this'?
+  var topLevelSelect = DOM.getChild('#main-style-select select', 0, this.tableRowNode);
+  var submenuSelects = DOM.getChildren('.tinymce-menu', this.tableRowNode);
+
+  // NOTE: Make event listener functions? How to handle functions executed on trigger?
   topLevelSelect.addEventListener('change', function() {
     var selectedOptionText = topLevelSelect.item(topLevelSelect.selectedIndex).innerText;
-    console.log(selectedOptionText);
-    that.menuDisplay('display', selectedOptionText, menuList);
+    that.menuDisplay(selectedOptionText, submenuSelects);
+    console.log(`THAT: `, that); // NOTE: 'that' refers to the main object instance/constructor
+    console.log(`THIS: `, this); // NOTE: 'this' refers to the element that fired the event
   });
-};
 
-// NOTE: Return the appended node for easier reference in other parts of code?
-TextEditor.prototype.__appendNode__ = function (parentNode, node) {
-  var lastChild = parentNode.lastChild.cloneNode(true);
-  parentNode.removeChild(parentNode.lastChild);
-  parentNode.appendChild(node);
-  parentNode.appendChild(lastChild);
+  submenuSelects.forEach( function(subMenu) {
+    var select = DOM.getChild(`#${subMenu.id} select`, 0, this.tableRowNode);
+    select.addEventListener('change', function() {
+      that.addContent(that.instance);
+    });
+  }, this);
 };
 
 TextEditor.prototype.__buildToplevel__ = function (enumerableList) {
-  var emmetString = 'td#main-style-select.tinymce-menu > select.mceNativeListBox > option {Select a Style}';
+  var emmetString = 'td#main-style-select > select.mceNativeListBox > option {Select a Style}';
   for (var item in enumerableList) { // First level
     if ( typeof enumerableList[item] === "object" ) {
       emmetString += ` + option {${enumerableList[item].title}}`; // Note blank space at start of String
@@ -65,7 +70,6 @@ TextEditor.prototype.__buildSublevels__ = function (enumerableList) {
     if ( typeof enumerableList[item] === "object" ) {
       for (var subItem in enumerableList[item]) { // Second level
         if ( typeof enumerableList[item][subItem] === "object" ) {
-          console.log(subItem);
           emmetString += ` + option {${enumerableList[item][subItem].title}}`; // Note blank space at start of String
         }
       }
@@ -75,22 +79,80 @@ TextEditor.prototype.__buildSublevels__ = function (enumerableList) {
   return emmetStringList;
 };
 
-TextEditor.prototype.menuDisplay = function (displayOption, target, nodeList) {
-  console.log(nodeList);
+// NOTE: Return the appended node for reference?
+TextEditor.prototype.__appendNode__ = function (parentNode, node) {
+  var lastChild = parentNode.lastChild.cloneNode(true);
+  parentNode.removeChild(parentNode.lastChild);
+  parentNode.appendChild(node);
+  parentNode.appendChild(lastChild);
+};
+
+// NOTE: Refactor menuDisplay to be more reusable? (i.e. can be applied to any menu element)
+TextEditor.prototype.menuDisplay = function (target, nodeList) {
   for (var node of nodeList) {
-    if ( node.id.includes( target.replace('Style ', '') ) ) { // NOTE: Use better condition
-      var nodeSelect = DOM.getChild(`#${node.id} select`, 0, this.tableRowNode);
-      console.log('BEFORE: ' + nodeSelect);
-      DOM.setStyle({display: 'display'}, nodeSelect);
-      console.log('AFTER: ' + nodeSelect);
+    var select = DOM.getChild(`#${node.id} select`, 0, this.tableRowNode);
+    if ( node.id.includes( target.replace('Style ', '') ) ) { // NOTE: Determine better condition?
+      DOM.setStyle({display: 'block'}, select);
     } else {
-      // DOM.setStyle({display: none}, node);
+      DOM.setStyle({display: 'none'}, select);
     }
   }
 };
 
-TextEditor.prototype.addContent = function () {
+TextEditor.prototype.addContent = function (tmceInstance) {
   // Adds HTML template to tinymce
+  MCE.setContent(tmceInstance, "This is some content...")
 };
+
+TextEditor.prototype.updateContent = function () {
+  // Updates HTML template in tinymce
+};
+
+TextEditor.prototype.getMenus = function () {
+  // Gets menu elements that were added to tinymce
+};
+
+TextEditor.prototype.setEventListener = function () { // NOTE: Makes sense as function?
+  // Adds an event listenser to elements in tinymce
+};
+
+TextEditor.prototype.__resetSelectMenu__ = function () {
+  // Resets a select menu to display the first/default option
+};
+
+TextEditor.prototype.saveTopLevelSelection = function () {
+  // Stores the main select boxe's selection. GM local storage?
+};
+
+TextEditor.prototype.sample = function () {
+  //
+};
+
+/**
+ * Helper functions
+ */
+ function escapeCharacters(string) {
+   var newString = CSS.escape(string);
+
+  //  var charactersToEscape= {
+  //    // '\.': '\\.', NOTE: Syntax to include '.' here instead of in separate if statement?
+  //    '/': '\\/',
+  //    '"': '\\"',
+  //    "'": "\\'"
+  //  };
+
+  //  if (newString.includes('\.')) {
+  //    newString = newString.replace( new RegExp(/[.]/, 'g'), '\\.');
+  //  }
+   //
+  //  for (char in charactersToEscape) {
+  //    if ( newString.includes(char) ) {
+  //      console.log(char);
+  //      newString = newString.replace(new RegExp(char, 'g'), charactersToEscape[char]);
+  //    }
+  //  }
+
+   return newString;
+ }
 
 export default TextEditor;
--- test.js
+++ test.js
@@ -1,14 +1,16 @@
-import Tinymce from './TinyMCE';
+import TinymceInterface from './TinyMCE';
 import Editor from './Editor';
 import { iconList } from './Icons';
 
 setTimeout(function() {
-  var MCE = new Tinymce();
+  var MCE = new TinymceInterface();
   var instances = MCE.getInstances();
   var icons = iconList;
+  // console.log(instances);
 
   instances.forEach( function(instance) {
     let editor = new Editor(instance);
+    console.log(editor);
     editor.buildMenu(icons);
     // editor.setEventListener();
   });
